e32d8a6b71e92ca935bf1d02388fb076d31968d8
ev0clu <ev0clu@gmail.com>
2023-06-06 14:34:59 +0200

    New POST request is added

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,30 @@ const messages = [
     {
         text: 'Hi there!',
         user: 'Amando',
-        added: dateFns.format(new Date(), 'MM/dd/yyy HH:mm')
+        added: '05.06.2023 15:31'
     },
     {
         text: 'Hello World!',
         user: 'Charles',
-        added: dateFns.format(new Date(), 'MM/dd/yyy HH:mm')
+        added: '05.06.2023 16:40'
     }
 ];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Mini message board', messages: messages });
+});
+
+/* POST new message*/
+router.post('/new', function (req, res, next) {
+    const messageText = req.body.messageText;
+    const userName = req.body.userName;
+    messages.push({
+        text: messageText,
+        user: userName,
+        added: dateFns.format(new Date(), 'MM/dd/yyy HH:mm')
+    });
+    res.redirect('/');
 });
 
 module.exports = router;
