bf0d5dd98896534e977b1ed1127cbf00d3e4610b
ev0clu <ev0clu@gmail.com>
2023-06-06 15:38:17 +0200

    Comment is updated

--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dateFns = require('date-fns');
 
-/* GET home page. */
+/* GET new message page */
 router.get('/', function (req, res, next) {
     res.render('form', { title: 'New message' });
 });
