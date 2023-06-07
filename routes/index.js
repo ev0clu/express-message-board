bc26f7ef5ac1a5a549ad5f7c7a0234aa93ccef32
ev0clu <ev0clu@gmail.com>
2023-06-07 18:48:00 +0200

    MongoDB handling with mongoose and dotenv is added

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,73 @@
 const express = require('express');
 const router = express.Router();
 const dateFns = require('date-fns');
+const mongoose = require('mongoose');
+const Message = require('../models/Message');
+require('dotenv').config();
 
-const messages = [
-    {
-        text: 'Hi there!',
-        user: 'Amando',
-        added: '05.06.2023 15:31'
-    },
-    {
-        text: 'Hello World!',
-        user: 'Charles',
-        added: '05.06.2023 16:40'
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set('strictQuery', false);
+
+// Define the database URL to connect to.
+const mongoDB = `mongodb+srv://ev0clu:${process.env.KEY}KEY@cluster0.6ywffwq.mongodb.net/mini_message_board?retryWrites=true&w=majority`;
+
+init();
+
+async function init() {
+    try {
+        await mongoose.connect(mongoDB);
+    } catch (err) {
+        console.log(err);
     }
-];
+}
+
+async function createMessage(text, user) {
+    try {
+        const newMessage = new Message({
+            text: text,
+            user: user
+        });
+        await newMessage.save();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getMessage() {
+    try {
+        const messages = await Message.find();
+
+        console.log('dd:', messages);
+
+        const updatedMessages = messages.map((message) => {
+            const formattedDate = dateFns.format(message.createdAt, 'MM/dd/yyy HH:mm');
+            return {
+                text: message._doc.text,
+                user: message._doc.user,
+                createdAt: formattedDate
+            };
+        });
+        console.log('arr:', updatedMessages);
+        return updatedMessages;
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Mini message board', messages: messages });
+router.get('/', async function (req, res, next) {
+    res.render('index', { title: 'Mini message board', messages: await getMessage() });
 });
 
 /* POST new message*/
-router.post('/new', function (req, res, next) {
+router.post('/new', async function (req, res, next) {
     const messageText = req.body.messageText;
     const userName = req.body.userName;
-    messages.push({
-        text: messageText,
-        user: userName,
-        added: dateFns.format(new Date(), 'MM/dd/yyy HH:mm')
-    });
+
+    await createMessage(messageText, userName);
+
     res.redirect('/');
 });
 
